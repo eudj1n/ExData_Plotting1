8813a7a51a13aef28c6f8b4a42c1b6df6eb06567
Евгений Козлов <eudjin@gmail.com>
2019-10-26 14:10:20 +0600

    Plot 2 - script and result

---
+++ plot2.R
@@ -0,0 +1,20 @@
+# plot2.png generation
+
+# load text data into table
+file <- "./household_power_consumption.txt"
+data <- read.table(file, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
+
+# make subset with defined date range
+subset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(subset$Date, subset$Time, sep = " "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subset$Global_active_power)
+
+# prepare graphic PNG device
+png("plot2.png", width=480, height=480)
+
+# make plot
+plot(datetime, globalActivePower, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+# close device
+dev.off()
